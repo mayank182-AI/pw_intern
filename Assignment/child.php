ce9b04b10fc119b8efb2e23443359902dba4ae10
MAYANK <mayankmishra33275@gmail.com>
2026-06-06 22:14:10 +0530

    Add files via upload

---
+++ Assignment/child.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1 style='color:green'>Child File</h1>";
+
+?>
---
+++ Assignment/include-file.php
@@ -0,0 +1,7 @@
+<?php
+// include ("./child.php");
+
+for($i=0;$i<=10;$i++){
+    include_once ("./child.php");
+}
+?>
