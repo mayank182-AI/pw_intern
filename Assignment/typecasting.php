274211eef9ee4585d4cb22b6f61bd893ee7fcb76
MAYANK <mayankmishra33275@gmail.com>
2026-06-06 15:37:02 +0530

    Add files via upload

---
+++ Assignment/typecasting.php
@@ -0,0 +1,23 @@
+<?php
+$a=5;
+$a=(string) $a;
+var_dump ($a);
+?>
+
+<?php
+$b="500";
+$b=(int) $b;
+var_dump ($b);
+?>
+
+<?php
+$c=500;
+$c=(bool) $c;
+var_dump ($c);
+?>
+
+<?php
+$d=10;
+$d=(array) $d;
+var_dump ($d);
+?>
